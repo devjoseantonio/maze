553344cbc31344300c4905037617c326253d2186
devjoseantonio <tonysoftco@gmail.com>
2023-11-16 07:01:13 -0500

    fixed out of the maze problems

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ MAP_HEIGHT = 15
 PLAYER_X = 0
 PLAYER_Y = 0
 
+
 def createmapobj():
     map_objects = []
     for x in range(10):
-        map_objects.append([randint(0, MAP_WIDTH), randint(0, MAP_HEIGHT)])
+        map_objects.append([randint(0, MAP_WIDTH - 1), randint(0, MAP_HEIGHT - 1)])
     return map_objects
+
+def removemapobj(map_object: list, map_objects: list):
+    map_objects.remove(map_object)
 
 def drawmap(plposx: int, plposy: int, map_objects: list):
     system("clear")
     print("Use wasd to move or q to end the game")
-    print("+" + "-"*(MAP_WIDTH*3 + 3) + "+")
+    print("+" + "-"*(MAP_WIDTH*3) + "+")
     for y in range(MAP_HEIGHT + 1):
         print("|", end="")
-        for x in range(MAP_WIDTH + 1 ):
+        for x in range(MAP_WIDTH ):
             map_object = [x, y]
             if map_object in map_objects:
                 if plposx == map_object[0] and plposy == map_object[1]:
                     print(" x ", end="")
-                    map_objects.remove(map_object)
+                    removemapobj(map_object, map_objects)
                 else:
                     print(" * ", end="")
             elif plposx == x and plposy == y:
@@ -33,7 +37,7 @@ def drawmap(plposx: int, plposy: int, map_objects: list):
             else:
                 print("   ", end="")
         print("|")
-    print("+" + "-"*(MAP_WIDTH*3 + 3) + "+")
+    print("+" + "-"*(MAP_WIDTH*3) + "+")
 
 def moveplayer  (player_x, player_y, map_objects):
     move = readchar()
@@ -46,7 +50,7 @@ def moveplayer  (player_x, player_y, map_objects):
         case "a":
             player_x -= 1
             if player_x < 0:
-                player_x = MAP_WIDTH
+                player_x = MAP_WIDTH -1
             
         case "s":
             player_y += 1
@@ -54,7 +58,7 @@ def moveplayer  (player_x, player_y, map_objects):
                 player_y = MAP_HEIGHT
         case "d":
             player_x += 1
-            if player_x > MAP_WIDTH:
+            if player_x > MAP_WIDTH - 1:
                 player_x = 0
         case "q":
             endgame = True
